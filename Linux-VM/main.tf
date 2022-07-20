ae3aeee04657fe6f2c999661b082ce63c401c985
BrandonErkamaa <brandonerkamaa@gmail.com>
2022-07-19 20:32:04 -0500

    Changes to linux-vm

--- Linux-VM/main.tf
+++ Linux-VM/main.tf
@@ -16,17 +16,17 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = "westus2"
+  location = "centralus"
   tags = {
-    Environment = "Terraform Getting Started"
-    Team = "DevOps"
+    Project = "Linux VM 1"
   }
 }
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
+  name                = "linux-vm-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "westus2"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = azurerm_resource_group.rg.tags
 }
 
