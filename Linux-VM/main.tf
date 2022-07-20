856f05acfd1ea69876a8e1edf4f4e67b2df35dcc
BrandonErkamaa <brandonerkamaa@gmail.com>
2022-07-19 22:23:51 -0500

    edit main.tf with subnet, NSG, rules

--- Linux-VM/main.tf
+++ Linux-VM/main.tf
@@ -30,3 +30,43 @@ resource "azurerm_virtual_network" "vnet" {
   tags                = azurerm_resource_group.rg.tags
 }
 
+resource "azurerm_subnet" "subnet" {
+  name                 = "vm-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = azurerm_resource_group.rg.tags
+}
+
+resource "azurerm_network_security_rule" "sec-rule" {
+  name                        = "vm-nsg-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "Fix this with sensitive variable"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg-association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resouce "azurerm_public_ip" "vm-ip" {
+  name                = "vm-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  tags                = azurerm_resource_group.rg.tags
+}
